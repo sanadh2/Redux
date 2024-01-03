e54127c8f22b9c2ac334f23576fe74e6bde60bf8
sanadh2 <suriyaakku@gmail.com>
2024-01-03 13:35:25 +0530

    redux extension

--- src/store.jsx
+++ src/store.jsx
@@ -4,11 +4,16 @@ import { accountReducer } from "./Components/Features/Account/AccountSlice";
 
 import { custoReducer } from "./Components/Features/Customers/customerSlice";
 
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: custoReducer,
 });
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
